e4c5a46cc2b7ca041fcadb6e5dcde30df75a7b60
0rakul0 <0rakul0render@gmail.com>
2021-04-13 23:52:10 -0300

    tela 36
    falta ajeitar o alinhamento do menu em baixo

--- src/pages/36/index.tsx
+++ src/pages/36/index.tsx
@@ -1,51 +1,261 @@
 import React from 'react'
-import { View, Text, TouchableOpacity, StyleSheet, Image } from 'react-native'
+import { View, Text, TouchableOpacity, StyleSheet, Image, ScrollView } from 'react-native'
 
 import backgroundImage from '../../grupou_assets/grafismos/grafismo_backgroud.png'
 
+import milton from '../../grupou_assets/img/milton.png';
+import rostoTriste from '../../grupou_assets/icones/faces_avaliacao/icone_face_2.png';
+import rostoNormal from '../../grupou_assets/icones/faces_avaliacao/icone_face_3.png';
+import rostoAlegre from '../../grupou_assets/icones/faces_avaliacao/icone_face_4.png';
+import rostoFeliz from '../../grupou_assets/icones/faces_avaliacao/icone_face_5.png';
+
+import likendin from '../../grupou_assets/icones/logo_linkedin.png'
+import instagram from '../../grupou_assets/icones/logo_instagram.png'
+import facebook from '../../grupou_assets/icones/logo_facebook.png'
+
+import dashboard from '../../grupou_assets/icones/icone_dashboard.png'
+import skills from '../../grupou_assets/icones/icone_skills.png'
+import perfil from '../../grupou_assets/icones/icone_perfil.png'
+import disciplina from '../../grupou_assets/icones/icone_disciplinas.png'
+import grupos from '../../grupou_assets/icones/icone_grupos.png'
+
+
+
 export default function Grupos() {
     return (
-        <View style={styles.container}>
-            <Image source={backgroundImage} style={styles.backImage}></Image>
-            <Text>Grupos</Text>
-            <View>
-                <View>
-                    <View>
-                        <Text>Gestão do Desing</Text>
-                        <Text>AV1</Text>
-                    </View>
-                    <TouchableOpacity>
+        <ScrollView >
+            <View style={styles.container}>
+                <Image source={backgroundImage} style={styles.backImage}></Image>
+                <Text style={styles.textoTitulo}>Grupos</Text>
+                <View style={styles.blocoGrupo}>
+                    <View style={styles.blocoTitulo}>
                         <View>
-                            <Text>
-                                Ver mais
+                            <Text style={styles.subTitulo}>Gestão do Desing</Text>
+                            <Text style={styles.textoNota}>AV1</Text>
+                        </View>
+                        <TouchableOpacity>
+                            <View style={styles.outLineBotao}>
+                                <Text style={styles.textoBotao}>
+                                    Ver mais
                             </Text>
-                        </View>
-                    </TouchableOpacity>
-                </View>
-                <View>
-                    <View>
-
+                            </View>
+                        </TouchableOpacity>
                     </View>
                     <View>
-                        <Text>Tarefas concluidas</Text>
+                        <View style={styles.rostos}>
+                            <Image source={milton} style={styles.imagem} />
+                            <Image source={rostoTriste} style={styles.imagem} />
+                            <Image source={rostoNormal} style={styles.imagem} />
+                            <Image source={rostoAlegre} style={styles.imagem} />
+                            <Image source={rostoFeliz} style={styles.imagem} />
+                        </View>
                         <View>
-                            <View></View>
-                            <Text>78%</Text>
+                            <Text style={styles.textoTarefas}>Tarefas concluidas</Text>
+                            <View style={styles.blocoExperience}>
+                                <View style={styles.experience}></View>
+                                <Text style={styles.textoExperience}>78%</Text>
+                            </View>
                         </View>
                     </View>
                 </View>
+
+                <View style={styles.blocoGrupo}>
+                    <View style={styles.blocoTitulo}>
+                        <View>
+                            <Text style={styles.subTitulo}>Gestão do Desing</Text>
+                            <Text style={styles.textoNota}>AV1</Text>
+                        </View>
+                        <TouchableOpacity>
+                            <View style={styles.outLineBotao}>
+                                <Text style={styles.textoBotao}>
+                                    Ver mais
+                            </Text>
+                            </View>
+                        </TouchableOpacity>
+                    </View>
+                    <View>
+                        <View style={styles.rostos}>
+                            <Image source={milton} style={styles.imagem} />
+                            <Image source={rostoTriste} style={styles.imagem} />
+                            <Image source={rostoNormal} style={styles.imagem} />
+                            <Image source={rostoAlegre} style={styles.imagem} />
+                            <Image source={rostoFeliz} style={styles.imagem} />
+                        </View>
+                        <View>
+                            <Text style={styles.textoTarefas}>Tarefas concluidas</Text>
+                            <View style={styles.blocoExperience}>
+                                <View style={styles.experience}></View>
+                                <Text style={styles.textoExperience}>78%</Text>
+                            </View>
+                        </View>
+                    </View>
+                </View>
+
+                <View style={styles.blocoRodape}>
+                    <View style={styles.rodape}>
+                        <Text style={styles.textoRodape}>Contato</Text>
+                        <Text style={styles.textoRodape}>grupou_contato@gmail.com</Text>
+                    </View>
+                    <View style={styles.blocoImagemRodape}>
+                        <Image source={likendin} style={styles.imagemRede} />
+                        <Image source={instagram} style={styles.imagemRede} />
+                        <Image source={facebook} style={styles.imagemRede} />
+                    </View>
+                    <View>
+                    </View>
+                   
+                </View>
+                <View style={styles.blocoMenuRodape}>
+                        <TouchableOpacity>
+                            <View>
+                                <Image source={dashboard} />
+                                <Text>Dashboard</Text>
+                            </View>
+                            </TouchableOpacity>
+                            <TouchableOpacity>
+                            <View>
+                                <Image source={grupos} />
+                                <Text>Grupos</Text>
+                            </View>
+                            
+                            </TouchableOpacity>
+                            <TouchableOpacity>
+                            <View>
+                                <Image source={disciplina} />
+                                <Text>Disciplina</Text>
+                            </View>
+                            
+                            </TouchableOpacity>
+                            <TouchableOpacity>
+                            <View>
+                                <Image source={skills} />
+                                <Text>Skills</Text>
+                            </View>
+                            
+                            </TouchableOpacity>
+                            <TouchableOpacity>
+                            <View>
+                                <Image source={perfil} />
+                                <Text>Perfil</Text>
+                            </View>
+                        </TouchableOpacity>
+                    </View>
             </View>
-            
-        </View>
+        </ScrollView>
     )
-}
+};
+
 const styles = StyleSheet.create({
     container: {
-      flex: 1,
-      marginTop: 30,
+        flex: 1,
+        marginTop: 30,
     },
-    backImage:{
-        position:'absolute',
-        backgroundColor:'#555ffe'
+    backImage: {
+        flex: 1,
+        position: 'absolute',
+        backgroundColor: '#555ffe'
+    },
+    textoTitulo: {
+        color: '#fff',
+        marginTop: 60,
+        margin: 40,
+        fontSize: 40,
+    },
+    blocoGrupo: {
+        borderRadius: 10,
+        margin: 15,
+        padding: 20,
+        backgroundColor: '#fff'
+    },
+    blocoTitulo: {
+        justifyContent: 'space-between',
+        flexDirection: 'row'
+    },
+    subTitulo: {
+        fontSize: 25,
+    },
+    textoNota: {
+        fontSize: 18,
+    },
+    outLineBotao: {
+        borderWidth: 1,
+        borderRadius: 4,
+        width: 100,
+        height: 40,
+        borderColor: '#f00',
+        justifyContent: 'center',
+        alignContent: 'center',
+        alignItems: 'center',
+    },
+    textoBotao: {
+        fontSize: 15,
+        color: '#f00'
+    },
+    rostos: {
+        marginTop: 20,
+        height: 65,
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+    },
+    imagem: {
+        borderRadius: 50,
+        width: 60,
+        height: 60
+    },
+    textoTarefas: {
+        color: '#888',
+        fontSize: 17,
+    },
+    blocoExperience: {
+        justifyContent: 'space-between',
+        alignContent: 'center',
+        alignItems: 'center',
+        flexDirection: 'row',
+
+    },
+    experience: {
+        width: 300,
+        height: 10,
+        borderRadius: 7,
+        backgroundColor: '#f55500'
+    },
+    textoExperience: {
+        fontSize: 15,
+        color: '#f55500',
+    },
+    blocoRodape:{
+        alignItems:'center',
+        position:'relative',
+        backgroundColor:'#600411',
+        paddingTop:20,
+    },
+    rodape:{
+        alignItems:'center',
+        justifyContent:'center',
+        marginBottom:20,
+    },
+    textoRodape:{
+        marginTop:5,
+        fontSize:20,
+        color:'#fff'
+    },
+    imagemRede:{
+        width:60,
+        height:60,
+        margin:10,
+    },
+    blocoImagemRodape:{
+        marginBottom:10,
+        flexDirection:'row',
+        flex:1,
+        justifyContent:'space-between',
+    },
+    blocoMenuRodape:{
+        padding:10,
+        backgroundColor:'#63005f',
+        flex:1,
+        flexDirection:'row',
+        justifyContent:'space-between',
     }
+
 })
